8abfbf6273d5f237c7d8be76b4c5e5c5e1d251c2
gregkorte <gryphionsroost@yahoo.com>
2016-05-08 10:03:52 -0500

    SPA: Iife exercise - walk-a-thon files setup up as per requirements, readme added.

---
+++ FrontEnd/single-page-applications/iife_walk-a-thon/scripts/donors.js
@@ -0,0 +1,18 @@
+'use-strict';
+
+var Walkathon = (function(walk){
+  let donors = {
+    name: 'John Donut',
+    pledge: 70
+  }
+
+  walk.getDonors = function(){
+    return donors;
+  }
+
+  walk.addDonor() = function(){
+
+  }
+
+  return walk;
+})()
