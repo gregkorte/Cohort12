f4bf0732902eb0449c47a0ca24c54f1abfd314a8
gregkorte <gryphionsroost@yahoo.com>
2016-05-09 19:15:20 -0500

    Wrote logic to handle both XHR calls, ready to build DOM elements.

--- FrontEnd/single-page-applications/xhr_pet-foods/food.js
+++ FrontEnd/single-page-applications/xhr_pet-foods/food.js
@@ -4,3 +4,33 @@ let output = document.getElementById('output');
 let store = ['dogs', 'cats'];
 let dogs;
 let cats;
+
+function getJson(file){
+  let request = new XMLHttpRequest()
+  request.addEventListener('load', function () {
+    switch(file){
+      case 'dogs.json':
+        dogs = JSON.parse(this.responseText);
+        buildDom(dogs.dog_brands);
+        return;
+      case 'cats.json':
+        cats = JSON.parse(this.responseText);
+        buildDom(cats.cat_brands);
+        return;
+    }
+  });
+  request.open('GET', file);
+  request.send();
+}
+
+function getFoods(){
+  for (var i = 0; i < store.length; i++){
+    getJson(`${store[i]}.json`);
+  }
+}
+
+function buildDom(food){
+  console.log(food);
+}
+
+getFoods();
