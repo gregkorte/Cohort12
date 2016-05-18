4968c360c855aba4e5b3f36e3a959d496cf5b2ae
gregkorte <gryphionsroost@yahoo.com>
2016-05-18 14:21:41 -0500

    Added callback to XHR, data passing through and available to callback.

--- FrontEnd/single-page-applications/q_spa/CarLot.js
+++ FrontEnd/single-page-applications/q_spa/CarLot.js
@@ -1,21 +1,22 @@
-var CarLot = (function () {
-  var inventory = [];
+var CarLot = (function() {
+  var inventory;
 
   return {
     getInventory: function () {
-
+      return inventory;
     },
-    loadInventory: function (callback) {
+    loadInventory: function (populatePage) {
       console.log('i\'m in!')
       var inventoryLoader = new XMLHttpRequest();
 
       inventoryLoader.addEventListener("load", function() {
         inventory = JSON.parse(this.responseText).cars;
-        console.log(inventory)
+        populatePage(inventory);
       });
       inventoryLoader.open('GET', 'inventory.json');
       inventoryLoader.send();
+
     }
   };
 
-})();
+})(CarLot || {});
--- FrontEnd/single-page-applications/q_spa/quiz.js
+++ FrontEnd/single-page-applications/q_spa/quiz.js
@@ -1,10 +1,16 @@
-function populatePage (inventory) {
-  // Loop over the inventory and populate the page
+'use-strict';
+var CarLot = (function(lot){
 
-  // Now that the DOM is loaded, establish all the event listeners needed
-  CarLot.activateEvents();
-}
+  function populatePage (inventory) {
+    console.log(inventory);
+    // Loop over the inventory and populate the page
 
-// Load the inventory and send a callback function to be
-// invoked after the process is complete
-CarLot.loadInventory();
+    // Now that the DOM is loaded, establish all the event listeners needed
+    // CarLot.activateEvents();
+  }
+
+  // Load the inventory and send a callback function to be
+  // invoked after the process is complete
+  CarLot.loadInventory(populatePage);
+
+})(CarLot || {});
