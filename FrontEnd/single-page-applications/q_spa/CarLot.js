086a3192d0c86bc1bf3bcccb4befb96700fca1c4
gregkorte <gryphionsroost@yahoo.com>
2016-05-18 14:07:03 -0500

    Added more objects to json file. XHR call pulling data back from json file and saving to private array.

--- FrontEnd/single-page-applications/q_spa/CarLot.js
+++ FrontEnd/single-page-applications/q_spa/CarLot.js
@@ -6,11 +6,15 @@ var CarLot = (function () {
 
     },
     loadInventory: function (callback) {
+      console.log('i\'m in!')
       var inventoryLoader = new XMLHttpRequest();
 
-      inventoryLoader.addEventListener("load", function () {
-
+      inventoryLoader.addEventListener("load", function() {
+        inventory = JSON.parse(this.responseText).cars;
+        console.log(inventory)
       });
+      inventoryLoader.open('GET', 'inventory.json');
+      inventoryLoader.send();
     }
   };
 
