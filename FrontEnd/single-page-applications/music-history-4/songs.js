1ab9aa6c8a673ebc7b4a660d5263e53e6dd20bf0
gregkorte <gryphionsroost@yahoo.com>
2016-05-28 21:06:07 -0500

    Adding XHR request sample code and created both json files.

--- FrontEnd/single-page-applications/music-history-4/songs.js
+++ FrontEnd/single-page-applications/music-history-4/songs.js
@@ -2,7 +2,7 @@ var songList = ['Dogs', 'Fields of Gold', 'Perpetual Change', 'Signify', 'Feathe
 var artistList = ['Pink Floyd', 'Sting', 'Yes', 'Porcupine Tree', 'Nujabes', 'Led Zeppelin', 'The Beatles', 'The Shins', 'Alice In Chains', 'Peter Gabriel'];
 var albumList = ['Animals', 'Ten Summoner\'s Tales', 'The Yes Album', 'Signify', 'Modal Soul', 'Led Zeppelin III', 'Revolver', 'Oh, Inverted World', 'Alice In Chains', 'So!'];
 
-var songs = [];
+// var songs = [];
 var newSongs = [];
 var output = document.getElementById('songOutput');
 var addMusicLink = document.getElementById('add-music-view-link');
@@ -17,17 +17,42 @@ var listAddSongs = document.getElementById('list-add-songs');
 var listAddArtists = document.getElementById('list-add-artists');
 var listAddAlbums = document.getElementById('list-add-albums');
 
-songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
-songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
-songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+// songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
+// songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
+// songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
+// songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
+// songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
 
-songs.push("Perpetual Change > by Yes on the album The Yes Album");
-songs.unshift("Sheep > by Pink Floyd on the album Animals");
+// songs.push("Perpetual Change > by Yes on the album The Yes Album");
+// songs.unshift("Sheep > by Pink Floyd on the album Animals");
+
+function executeThisCodeAfterFileIsLoaded () {
+  // A bit more about what `this` is here. What is the execution context?
+  console.log(this.responseText);
+
+  // Show usage of JSON.parse() here to get a POJO
+  populateSongs(JSON.parse(this.responseText).music)
+}
+
+function executeThisCodeIfXHRFails(){
+  console.log('XHR call failed')
+}
+
+// Create an XHR object
+var myRequest = new XMLHttpRequest();
+
+// XHR objects emit events when their operation is complete, or an error occurs
+myRequest.addEventListener("load", executeThisCodeAfterFileIsLoaded);
+myRequest.addEventListener("error", executeThisCodeIfXHRFails);
+
+// Then tell the XHR object exactly what to do
+myRequest.open("GET", "songs1.json");
+
+// Tell the XHR object to start
+myRequest.send();
 
 
-function populateSongs(){
+function populateSongs(songs){
   output.innerHTML = '';
   for (var i = 0; i < songs.length; i++){
     var newSong = songs[i].replace(/[(*@!]/g, '').replace('>', '-');
@@ -196,8 +221,8 @@ addSongBtn.addEventListener('click', function(){
   addAlbum.value = '';
   addArtist.value = '';
   listElements();
-  populateSongs();
+  // populateSongs();
 })
 
-populateSongs();
+// populateSongs();
 
