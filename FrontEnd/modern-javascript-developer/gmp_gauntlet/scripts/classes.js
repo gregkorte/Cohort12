c0e339cf0d9f1d04a6aca73334494872c856a815
gregkorte <gryphionsroost@yahoo.com>
2016-09-11 11:59:08 -0500

    Added initial stealth classes.

--- FrontEnd/modern-javascript-developer/gmp_gauntlet/scripts/classes.js
+++ FrontEnd/modern-javascript-developer/gmp_gauntlet/scripts/classes.js
@@ -123,3 +123,37 @@ Gauntlet.GuildHall.Sorcerer.prototype = new Gauntlet.GuildHall.Mage();
       - Ninja
       - Assassin
  */
+Gauntlet.GuildHall.Rogue = function() {
+  this.name = "Rogue";
+  this.magical = true;
+  this.healthBonus = this.healthBonus - 10;
+  this.strengthBonus = this.strengthBonus - 20;
+  this.intelligenceBonus = this.intelligenceBonus + 20;
+};
+Gauntlet.GuildHall.Rogue.prototype = new Gauntlet.GuildHall.PlayerClass();
+
+
+Gauntlet.GuildHall.Thief = function() {
+  this.name = "Thief";
+  this.healthBonus = this.healthBonus + 5;
+  this.strengthBonus = this.strengthBonus - 10;
+  this.intelligenceBonus = this.intelligenceBonus + 20;
+};
+Gauntlet.GuildHall.Thief.prototype = new Gauntlet.GuildHall.Rogue();
+
+
+Gauntlet.GuildHall.Ninja = function() {
+  this.name = "Ninja";
+  this.healthBonus = this.healthBonus - 15;
+  this.strengthBonus = this.strengthBonus - 25;
+  this.intelligenceBonus = this.intelligenceBonus + 40;
+};
+Gauntlet.GuildHall.Ninja.prototype = new Gauntlet.GuildHall.Rogue();
+
+
+Gauntlet.GuildHall.Assassin = function() {
+  this.name = "Assassin";
+  this.strengthBonus = this.strengthBonus - 10;
+  this.intelligenceBonus = this.intelligenceBonus + 10;
+};
+Gauntlet.GuildHall.Assassin.prototype = new Gauntlet.GuildHall.Rogue();
