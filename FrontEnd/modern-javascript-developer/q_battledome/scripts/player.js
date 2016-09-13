8af3892010bc5f262d53a021200254c522708fa0
gregkorte <gryphionsroost@yahoo.com>
2016-09-13 08:42:07 -0500

    Removed setWeapon and setArena methods from Player constructor.

--- FrontEnd/modern-javascript-developer/q_battledome/scripts/player.js
+++ FrontEnd/modern-javascript-developer/q_battledome/scripts/player.js
@@ -6,23 +6,14 @@ var Battledome = (function(battledome){
 
   battledome.Combatants.Player = function(name){
     this.bot = '';
-    this.weapon = '';
-    this.mod = '';
     this.name = name;
     this.wins;
     this.loses;
+    this.botHangar;
   }
 
   battledome.Combatants.Player.prototype.setBot = function(botType){
     this.bot = new Battledome.BotLab[botType];
-  }
-
-  battledome.Combatants.Player.prototype.setWeapon = function(weaponType){
-    this.weapon = weaponType;
-  }
-
-  battledome.Combatants.Player.prototype.setMod = function(weaponType){
-    this.mod = modType;
   }
 
   return battledome;
