653a97a8a78db59c768d2db21f25d0d5b176e584
gregkorte <gryphionsroost@yahoo.com>
2016-09-13 08:38:01 -0500

    Added dynamic background functionality.

--- FrontEnd/modern-javascript-developer/q_battledome/scripts/main.js
+++ FrontEnd/modern-javascript-developer/q_battledome/scripts/main.js
@@ -2,10 +2,29 @@
 
 var Battledome = (function(battledome){
 
-battledome.Arena = function(){
-  this.background;
-  this.champion;
-}
+  // TRY TO GET THIS DONE WITH AJAX CALL TO JSON FILE //
+  let bgObject = {
+    "Space": ["assets/terra.jpg", "assets/space.jpg", "assets/interior.jpg"],
+    "Terran": ["assets/terra.jpg"],
+    "Interior": ["assets/interior.jpg"]
+  }
+
+  battledome.Arena = function(){
+    this.background = '';
+    this.player1 = '';
+    this.player2 = '';
+    this.champion;
+  }
+
+  battledome.Arena.prototype.setBg = function(bgType){
+    var bg = bgObject[bgType];
+    var newBg = bg[Math.floor(Math.random()*bg.length)];
+    this.background = newBg;
+  }
+
+  battledome.Arena.prototype.setPlayer = function(player, name){
+    this[player] = new Battledome.Combatants.Player(name);
+  }
 
 return battledome;
 
