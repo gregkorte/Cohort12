09bf9ba770a5164ab020432bac10bbba2c9b279e
gregkorte <gryphionsroost@yahoo.com>
2016-09-13 08:44:05 -0500

    Testing new constructor functionality, regrouped all objects to be composed within the Arena constructor object.

--- FrontEnd/modern-javascript-developer/q_battledome/scripts/app.js
+++ FrontEnd/modern-javascript-developer/q_battledome/scripts/app.js
@@ -1,8 +1,23 @@
 'use strict';
 
-var r1 = new Battledome.Combatants.Player('Titan');
+var arena = new Battledome.Arena();
 
-r1.setBot('Gort')
+arena.setBg('Space');
 
-console.log(r1)
-console.log(Battledome)
+arena.setPlayer('player1', 'Titan');
+arena.setPlayer('player2', 'Strange');
+
+arena.player1.setBot('Gort');
+arena.player2.setBot('TX');
+
+console.log(arena)
+
+// var r1 = new Battledome.Combatants.Player('Titan');
+
+// r1.setBot('Gort')
+// r1.setArena('Space')
+// r1.bot.setWeapon('SunDancer')
+// r1.bot.setMod('ForceField')
+
+// console.log(r1)
+// console.log(Battledome)
