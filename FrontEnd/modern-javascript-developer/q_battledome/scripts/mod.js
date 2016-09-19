132dd8cc73fffb7a1a788bc31408f608d35ba4e5
gregkorte <gryphionsroost@yahoo.com>
2016-09-19 09:37:32 -0500

    Fleshed out weapons and mods constructors and prototyping.

--- FrontEnd/modern-javascript-developer/q_battledome/scripts/mod.js
+++ FrontEnd/modern-javascript-developer/q_battledome/scripts/mod.js
@@ -5,8 +5,130 @@ var Battledome = (function(battledome){
   battledome.SciFacility = {};
 
   battledome.SciFacility.Mod = function(){
-    this.defense = 1;
+    this.defenseDuration = 0;
+    this.defenseType = '';
+    this.damageReduction = 0;
   }
+
+  battledome.SciFacility.AccessControl = function(){
+    this.defenseDuration = 3;
+    this.defenseType = '';
+    this.damageReduction = 1;
+    this.description = '';
+    this.name = '';
+  }
+
+  battledome.SciFacility.AccessControl.prototype = new battledome.SciFacility.Mod();
+
+  battledome.SciFacility.SurgeShield = function(){
+    this.defenseDuration = 3;
+    this.defenseType = '';
+    this.damageReduction = 1;
+    this.description = '';
+    this.name = '';
+  }
+
+  battledome.SciFacility.SurgeShield.prototype = new battledome.SciFacility.Mod();
+
+  battledome.SciFacility.AcquireFunction = function(){
+    this.defenseDuration = 3;
+    this.defenseType = '';
+    this.damageReduction = 1;
+    this.description = '';
+    this.name = '';
+  }
+
+  battledome.SciFacility.AcquireFunction.prototype = new battledome.SciFacility.Mod();
+
+  battledome.SciFacility.XRayVision = function(){
+    this.defenseDuration = 3;
+    this.defenseType = '';
+    this.damageReduction = 1;
+    this.description = '';
+    this.name = '';
+  }
+
+  battledome.SciFacility.XRayVision.prototype = new battledome.SciFacility.Mod();
+
+  battledome.SciFacility.SurgeFilter = function(){
+    this.defenseDuration = 3;
+    this.defenseType = '';
+    this.damageReduction = 1;
+    this.description = '';
+    this.name = '';
+  }
+
+  battledome.SciFacility.SurgeFilter.prototype = new battledome.SciFacility.Mod();
+
+  battledome.SciFacility.MagneticField = function(){
+    this.defenseDuration = 3;
+    this.defenseType = '';
+    this.damageReduction = 1;
+    this.description = '';
+    this.name = '';
+  }
+
+  battledome.SciFacility.MagneticField.prototype = new battledome.SciFacility.Mod();
+
+  battledome.SciFacility.ThermoOpticCamouflage = function(){
+    this.defenseDuration = 3;
+    this.defenseType = '';
+    this.damageReduction = 1;
+    this.description = '';
+    this.name = '';
+  }
+
+  battledome.SciFacility.ThermoOpticCamouflage.prototype = new battledome.SciFacility.Mod();
+
+  battledome.SciFacility.NervRegen = function(){
+    this.defenseDuration = 3;
+    this.defenseType = '';
+    this.damageReduction = 1;
+    this.description = '';
+    this.name = '';
+  }
+
+  battledome.SciFacility.NervRegen.prototype = new battledome.SciFacility.Mod();
+
+  battledome.SciFacility.ATField = function(){
+    this.defenseDuration = 3;
+    this.defenseType = '';
+    this.damageReduction = 1;
+    this.description = '';
+    this.name = '';
+  }
+
+  battledome.SciFacility.ATField.prototype = new battledome.SciFacility.Mod();
+
+  battledome.SciFacility.AudioSensor = function(){
+    this.defenseDuration = 3;
+    this.defenseType = '';
+    this.damageReduction = 1;
+    this.description = '';
+    this.name = '';
+  }
+
+  battledome.SciFacility.AudioSensor.prototype = new battledome.SciFacility.Mod();
+
+  battledome.SciFacility.AppendageArmor = function(){
+    this.defenseDuration = 3;
+    this.defenseType = '';
+    this.damageReduction = 1;
+    this.description = '';
+    this.name = '';
+  }
+
+  battledome.SciFacility.AppendageArmor.prototype = new battledome.SciFacility.Mod();
+
+  battledome.SciFacility.Intelliscan = function(){
+    this.defenseDuration = 3;
+    this.defenseType = '';
+    this.damageReduction = 1;
+    this.description = '';
+    this.name = '';
+  }
+
+  battledome.SciFacility.Intelliscan.prototype = new battledome.SciFacility.Mod();
 
   return battledome;
 
--- FrontEnd/modern-javascript-developer/q_battledome/scripts/weapon.js
+++ FrontEnd/modern-javascript-developer/q_battledome/scripts/weapon.js
@@ -4,9 +4,328 @@ var Battledome = (function(battledome){
 
   battledome.Armory = {};
 
-  battledome.Armory.Weapon = function(){
-    this.attack = 1;
+  function damageRoll(max, min){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
+
+  battledome.Armory.Weapon = function(){
+    this.attackDuration = 0;
+    this.attackPoints = 10;
+    this.attackPower = 0;
+    this.damage = 0;
+  }
+
+  battledome.Armory.SentryWave = function(){
+    this.attackDuration = 3;
+    this.attackPoints -= 3;
+    this.attackPower = 20;
+    this.attackType = '';
+    this.damage = damageRoll(x, y);
+    this.description = '';
+    this.name = '';
+    this.toSave = '';
+  }
+
+  battledome.Armory.SentryWave.prototype = new Battledome.Armory.Weapon();
+
+  battledome.Armory.PowerKill = function(){
+    this.attackDuration = 3;
+    this.attackPoints -= 3;
+    this.attackPower = 20;
+    this.attackType = '';
+    this.damage = damageRoll(x, y);
+    this.description = '';
+    this.name = '';
+    this.toSave = '';
+  }
+
+  battledome.Armory.PowerKill.prototype = new Battledome.Armory.Weapon();
+
+  battledome.Armory.AirLock = function(){
+    this.attackDuration = 3;
+    this.attackPoints -= 3;
+    this.attackPower = 20;
+    this.attackType = '';
+    this.damage = damageRoll(x, y);
+    this.description = '';
+    this.name = '';
+    this.toSave = '';
+  }
+
+  battledome.Armory.AirLock.prototype = new Battledome.Armory.Weapon();
+
+  battledome.Armory.DeRez = function(){
+    this.attackDuration = 3;
+    this.attackPoints -= 3;
+    this.attackPower = 20;
+    this.attackType = '';
+    this.damage = damageRoll(x, y);
+    this.description = '';
+    this.name = '';
+    this.toSave = '';
+  }
+
+  battledome.Armory.DeRez.prototype = new Battledome.Armory.Weapon();
+
+  battledome.Armory.AcidGas = function(){
+    this.attackDuration = 3;
+    this.attackPoints -= 3;
+    this.attackPower = 20;
+    this.attackType = '';
+    this.damage = damageRoll(x, y);
+    this.description = '';
+    this.name = '';
+    this.toSave = '';
+  }
+
+  battledome.Armory.AcidGas.prototype = new Battledome.Armory.Weapon();
+
+  battledome.Armory.LightDisc = function(){
+    this.attackDuration = 3;
+    this.attackPoints -= 3;
+    this.attackPower = 20;
+    this.attackType = '';
+    this.damage = damageRoll(x, y);
+    this.description = '';
+    this.name = '';
+    this.toSave = '';
+  }
+
+  battledome.Armory.LightDisc.prototype = new Battledome.Armory.Weapon();
+
+  battledome.Armory.RushKick = function(){
+    this.attackDuration = 3;
+    this.attackPoints -= 3;
+    this.attackPower = 20;
+    this.attackType = '';
+    this.damage = damageRoll(x, y);
+    this.description = '';
+    this.name = '';
+    this.toSave = '';
+  }
+
+  battledome.Armory.RushKick.prototype = new Battledome.Armory.Weapon();
+
+  battledome.Armory.LeapPunch = function(){
+    this.attackDuration = 3;
+    this.attackPoints -= 3;
+    this.attackPower = 20;
+    this.attackType = '';
+    this.damage = damageRoll(x, y);
+    this.description = '';
+    this.name = '';
+    this.toSave = '';
+  }
+
+  battledome.Armory.LeapPunch.prototype = new Battledome.Armory.Weapon();
+
+  battledome.Armory.NanoDisrupter = function(){
+    this.attackDuration = 3;
+    this.attackPoints -= 3;
+    this.attackPower = 20;
+    this.attackType = '';
+    this.damage = damageRoll(x, y);
+    this.description = '';
+    this.name = '';
+    this.toSave = '';
+  }
+
+  battledome.Armory.NanoDisrupter.prototype = new Battledome.Armory.Weapon();
+
+  battledome.Armory.UploadVirus = function(){
+    this.attackDuration = 3;
+    this.attackPoints -= 3;
+    this.attackPower = 20;
+    this.attackType = '';
+    this.damage = damageRoll(x, y);
+    this.description = '';
+    this.name = '';
+    this.toSave = '';
+  }
+
+  battledome.Armory.UploadVirus.prototype = new Battledome.Armory.Weapon();
+
+  battledome.Armory.EmpGenerator = function(){
+    this.attackDuration = 3;
+    this.attackPoints -= 3;
+    this.attackPower = 20;
+    this.attackType = '';
+    this.damage = damageRoll(x, y);
+    this.description = '';
+    this.name = '';
+    this.toSave = '';
+  }
+
+  battledome.Armory.EmpGenerator.prototype = new Battledome.Armory.Weapon();
+
+  battledome.Armory.PlasmaCannon = function(){
+    this.attackDuration = 3;
+    this.attackPoints -= 3;
+    this.attackPower = 20;
+    this.attackType = '';
+    this.damage = damageRoll(x, y);
+    this.description = '';
+    this.name = '';
+    this.toSave = '';
+  }
+
+  battledome.Armory.PlasmaCannon.prototype = new Battledome.Armory.Weapon();
+
+  battledome.Armory.GhostHack = function(){
+    this.attackDuration = 3;
+    this.attackPoints -= 3;
+    this.attackPower = 20;
+    this.attackType = '';
+    this.damage = damageRoll(x, y);
+    this.description = '';
+    this.name = '';
+    this.toSave = '';
+  }
+
+  battledome.Armory.GhostHack.prototype = new Battledome.Armory.Weapon();
+
+  battledome.Armory.LightBlade = function(){
+    this.attackDuration = 3;
+    this.attackPoints -= 3;
+    this.attackPower = 20;
+    this.attackType = '';
+    this.damage = damageRoll(x, y);
+    this.description = '';
+    this.name = '';
+    this.toSave = '';
+  }
+
+  battledome.Armory.LightBlade.prototype = new Battledome.Armory.Weapon();
+
+  battledome.Armory.SonicGlaive = function(){
+    this.attackDuration = 3;
+    this.attackPoints -= 3;
+    this.attackPower = 20;
+    this.attackType = '';
+    this.damage = damageRoll(x, y);
+    this.description = '';
+    this.name = '';
+    this.toSave = '';
+  }
+
+  battledome.Armory.SonicGlaive.prototype = new Battledome.Armory.Weapon();
+
+  battledome.Armory.ElectroCrossbow = function(){
+    this.attackDuration = 3;
+    this.attackPoints -= 3;
+    this.attackPower = 20;
+    this.attackType = '';
+    this.damage = damageRoll(x, y);
+    this.description = '';
+    this.name = '';
+    this.toSave = '';
+  }
+
+  battledome.Armory.ElectroCrossbow.prototype = new Battledome.Armory.Weapon();
+
+  battledome.Armory.PositronRifle = function(){
+    this.attackDuration = 3;
+    this.attackPoints -= 3;
+    this.attackPower = 20;
+    this.attackType = '';
+    this.damage = damageRoll(x, y);
+    this.description = '';
+    this.name = '';
+    this.toSave = '';
+  }
+
+  battledome.Armory.PositronRifle.prototype = new Battledome.Armory.Weapon();
+
+  battledome.Armory.ZastavaCz100 = function(){
+    this.attackDuration = 3;
+    this.attackPoints -= 3;
+    this.attackPower = 20;
+    this.attackType = '';
+    this.damage = damageRoll(x, y);
+    this.description = '';
+    this.name = '';
+    this.toSave = '';
+  }
+
+  battledome.Armory.ZastavaCz100.prototype = new Battledome.Armory.Weapon();
+
+  battledome.Armory.TowBomb = function(){
+    this.attackDuration = 3;
+    this.attackPoints -= 3;
+    this.attackPower = 20;
+    this.attackType = '';
+    this.damage = damageRoll(x, y);
+    this.description = '';
+    this.name = '';
+    this.toSave = '';
+  }
+
+  battledome.Armory.TowBomb.prototype = new Battledome.Armory.Weapon();
+
+  battledome.Armory.PlasmaBeam = function(){
+    this.attackDuration = 3;
+    this.attackPoints -= 3;
+    this.attackPower = 20;
+    this.attackType = '';
+    this.damage = damageRoll(x, y);
+    this.description = '';
+    this.name = '';
+    this.toSave = '';
+  }
+
+  battledome.Armory.PlasmaBeam.prototype = new Battledome.Armory.Weapon();
+
+  battledome.Armory.NaniteStream = function(){
+    this.attackDuration = 3;
+    this.attackPoints -= 3;
+    this.attackPower = 20;
+    this.attackType = '';
+    this.damage = damageRoll(x, y);
+    this.description = '';
+    this.name = '';
+    this.toSave = '';
+  }
+
+  battledome.Armory.NaniteStream.prototype = new Battledome.Armory.Weapon();
+
+  battledome.Armory.NanoInfection = function(){
+    this.attackDuration = 3;
+    this.attackPoints -= 3;
+    this.attackPower = 20;
+    this.attackType = '';
+    this.damage = damageRoll(x, y);
+    this.description = '';
+    this.name = '';
+    this.toSave = '';
+  }
+
+  battledome.Armory.NanoInfection.prototype = new Battledome.Armory.Weapon();
+
+  battledome.Armory.TentacleJolt = function(){
+    this.attackDuration = 3;
+    this.attackPoints -= 3;
+    this.attackPower = 20;
+    this.attackType = '';
+    this.damage = damageRoll(x, y);
+    this.description = '';
+    this.name = '';
+    this.toSave = '';
+  }
+
+  battledome.Armory.TentacleJolt.prototype = new Battledome.Armory.Weapon();
+
+  battledome.Armory.MatterSiphon = function(){
+    this.attackDuration = 3;
+    this.attackPoints -= 3;
+    this.attackPower = 20;
+    this.attackType = '';
+    this.damage = damageRoll(x, y);
+    this.description = '';
+    this.name = '';
+    this.toSave = '';
+  }
+
+  battledome.Armory.MatterSiphon.prototype = new Battledome.Armory.Weapon();
 
   return battledome;
 
