8e8fd79d86190b1ddce8febb3c49bd5ab3923b6e
gregkorte <gryphionsroost@yahoo.com>
2016-09-19 08:50:13 -0500

    Completed bot types and prototyping.

--- FrontEnd/modern-javascript-developer/q_battledome/scripts/robot.js
+++ FrontEnd/modern-javascript-developer/q_battledome/scripts/robot.js
@@ -6,143 +6,189 @@ var Battledome = (function(battledome){
 
   battledome.BotLab.Robot = function(){
     this.name = "Maximillian's Shell";
-    this.healthBonus = 0;
-    this.strengthBonus = 0;
-    this.weapon = '';
-    this.mod = '';
+    this.health = 10;
+    this.adaptability = 10;
+    this.attackPts = 10;
 
     this.toString = function() {
       return this.name;
     }
   }
 
-  battledome.BotLab.Robot.prototype.setWeapon = function(weaponType){
-    this.weapon = weaponType;
-  }
-
-  battledome.BotLab.Robot.prototype.setMod = function(modType){
-    this.mod = modType;
-  }
-
   battledome.BotLab.Amorphous = function(){
-    this.healthBonus += 5;
-    this.speedBonus = 5;
-    this.aiBonus = 30;
-    this.stealthBonus = 30;
+    this.health += 10;
+    this.adaptablility += 50;
+    this.ai = 30;
+    this.stealth = 30;
+    this.sentience = 50;
+
+    this.WeaponsAllowed = ['SentryWave', 'LockOut', 'AirLock', 'DeRez', 'AcidGas', 'LightDisc']
   }
 
   battledome.BotLab.Amorphous.prototype = new Battledome.BotLab.Robot();
 
+  battledome.BotLab.Amorphous.prototype.setWeapon = function(weaponType){
+    this.weapon = new Battledome.Armory[weaponType];
+  }
+
+  battledome.BotLab.Amorphous.prototype.setMod = function(modType){
+    this.mod = modType;
+  }
+
   battledome.BotLab.Hal9000 = function(){
-    this.adaptablilityBonus = 50;
-    this.sentientBonus = 0;
+    this.ai += 30;
+    this.stealth += 30;
+    this.sentience += 50;
+    this.malice = '';
   }
 
   battledome.BotLab.Hal9000.prototype = new Battledome.BotLab.Amorphous();
 
   battledome.BotLab.MCP = function(){
-    this.adaptabilityBonus = 25;
-    this.sentientBonus = 0;
+    this.ai += 20;
+    this.stealth -= 5;
+    this.sentience += 40;
+    this.pride = '';
   }
 
   battledome.BotLab.MCP.prototype = new Battledome.BotLab.Amorphous();
 
   battledome.BotLab.RedQueen = function(){
-    this.adaptabilityBonus = 40;
-    this.sentientBonus = 0;
+    this.ai += 25;
+    this.stealth += 15;
+    this.sentience += 25;
+    this.fear = '';
   }
 
   battledome.BotLab.RedQueen.prototype = new Battledome.BotLab.Amorphous();
 
   battledome.BotLab.Android = function(){
-    this.healthBonus += 10;
-    this.speedBonus = 20;
-    this.aiBonus = 20;
-    this.strengthBonus += 15;
+    this.health += 10;
+    this.speed = 20;
+    this.ai = 20;
+    this.strength += 15;
+    this.combatMode = '';
+
+    this.WeaponsAllowed = ['RushKick', 'LeapPunch', 'NanoDisruptor', 'UploadVirus', 'LAPD2019Blaster', 'EMPGenerator']
   }
 
-  battledome.BotLab.Android.prototype = new Battledome.BotLab.Robot();
+  battledome.BotLab.Droid.prototype = new Battledome.BotLab.Robot();
 
-  battledome.BotLab.NEXUS6 = function(){
-    this.adaptablilityBonus = 50;
-    this.mobilityBonus = 15;
+  battledome.BotLab.Android.prototype.setWeapon = function(weaponType){
+    this.weapon = weaponType;
   }
 
-  battledome.BotLab.NEXUS6.prototype = new Battledome.BotLab.Android();
+  battledome.BotLab.Amee = function(){
+    this.adaptablility = 50;
+    this.mobility = 15;
+    this.perception = '';
+  }
+
+  battledome.BotLab.Amee.prototype = new Battledome.BotLab.Droid();
 
   battledome.BotLab.TX = function(){
-    this.adaptabilityBonus = 25;
-    this.mobilityBonus = 15;
+    this.adaptability = 25;
+    this.mobility = 15;
+    this.bloodLust = '';
   }
 
-  battledome.BotLab.TX.prototype = new Battledome.BotLab.Android();
+  battledome.BotLab.TX.prototype = new Battledome.BotLab.Droid();
 
   battledome.BotLab.NS5 = function(){
-    this.adaptabilityBonus = 40;
-    this.mobilityBonus = 15;
+    this.adaptability = 40;
+    this.mobility = 15;
+    this.ferocity = '';
   }
 
-  battledome.BotLab.NS5.prototype = new Battledome.BotLab.Android();
+  battledome.BotLab.NS5.prototype = new Battledome.BotLab.Droid();
 
   battledome.BotLab.Cyborg = function(){
-    this.healthBonus += 20;
-    this.speedBonus = 10;
-    this.aiBonus = 10;
-    this.strengthBonus += 30;
+    this.health += 20;
+    this.speed = 10;
+    this.ai = 10;
+    this.strength += 30;
+    this.assault = '';
+
+    this.WeaponsAllowed = ['GhostHack', 'Lightsaber', 'SonicGlaive', 'SuperElectroCrossbow', 'PositronRifle', 'ZastavaCz100']
   }
 
   battledome.BotLab.Cyborg.prototype = new Battledome.BotLab.Robot();
 
+  battledome.BotLab.Cyborg.prototype.setWeapon = function(weaponType){
+    this.weapon = weaponType;
+  }
+
+  battledome.BotLab.Cyborg.prototype.setMod = function(modType){
+    this.mod = modType;
+  }
+
   battledome.BotLab.Project2501 = function(){
-    this.adaptablilityBonus = 50;
-    this.mobilityBonus = 15;
+    this.adaptablility = 50;
+    this.mobility = 15;
+    this.infiltration = '';
   }
 
   battledome.BotLab.Project2501.prototype = new Battledome.BotLab.Cyborg();
 
   battledome.BotLab.Grevious = function(){
-    this.adaptabilityBonus = 25;
-    this.mobilityBonus = 15;
+    this.adaptability = 25;
+    this.mobility = 15;
+    this.darkSide = '';
   }
 
   battledome.BotLab.Grevious.prototype = new Battledome.BotLab.Cyborg();
 
   battledome.BotLab.EVA = function(){
-    this.adaptabilityBonus = 40;
-    this.mobilityBonus = 15;
+    this.adaptability = 40;
+    this.mobility = 15;
+    this.berserk = '';
   }
 
   battledome.BotLab.EVA.prototype = new Battledome.BotLab.Cyborg();
 
   battledome.BotLab.Swarm = function(){
-    this.healthBonus += 30;
-    this.speedBonus = 30;
-    this.aiBonus = 20;
-    this.stealthBonus = 15;
+    this.health += 30;
+    this.speed = 30;
+    this.ai = 20;
+    this.stealth = 15;
+    this.damage = '';
+
+    this.WeaponsAllowed = ['TowBomb', 'PlasmaBeam', 'NaniteStream', 'NanoInfection', 'TentacleJolt', '']
   }
 
   battledome.BotLab.Swarm.prototype = new Battledome.BotLab.Robot();
 
+  battledome.BotLab.Swarm.prototype.setWeapon = function(weaponType){
+    this.weapon = weaponType;
+  }
+
+  battledome.BotLab.Swarm.prototype.setMod = function(modType){
+    this.mod = modType;
+  }
+
   battledome.BotLab.Sentinel = function(){
-    this.adaptablilityBonus = 50;
-    this.mobilityBonus = 15;
+    this.adaptablility = 20;
+    this.mobility = 15;
+    this.pursuit = '';
   }
 
   battledome.BotLab.Sentinel.prototype = new Battledome.BotLab.Swarm();
 
   battledome.BotLab.Gort = function(){
-    this.adaptabilityBonus = 25;
-    this.mobilityBonus = 15;
+    this.adaptability = 25;
+    this.mobility = 15;
+    this.overwhelm = '';
   }
 
   battledome.BotLab.Gort.prototype = new Battledome.BotLab.Swarm();
 
-  battledome.BotLab.Spider = function(){
-    this.adaptabilityBonus = 40;
-    this.mobilityBonus = 15;
+  battledome.BotLab.Microbot = function(){
+    this.adaptability = 40;
+    this.mobility = 15;
+    this. shapeshift = '';
   }
 
-  battledome.BotLab.Spider.prototype = new Battledome.BotLab.Swarm();
+  battledome.BotLab.Microbot.prototype = new Battledome.BotLab.Swarm();
 
   return battledome;
 
